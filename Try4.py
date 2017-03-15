70b351d473439cddc1292ed5d078332716317997
frankestein123toinfinity <saha.rajarshi99@gmail.com>
2017-03-15 14:38:46 +0530

    Try1

---
+++ Try4.py
@@ -0,0 +1,12 @@
+import requests, bs4, time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('http://www.lyricsondemand.com/p/pinkfloydlyrics/comfortablynumblyrics.html')
+res = requests.get('http://www.lyricsondemand.com/p/pinkfloydlyrics/comfortablynumblyrics.html')
+
+noStarchSoup = bs4.BeautifulSoup(res.text, "html.parser")
+pElems = noStarchSoup.select('.lcontent')
+print(pElems[0].getText())
+
+time.sleep(5)
